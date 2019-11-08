4a7e3c1d4d56ee3c6dca9091d4ee3b7acef8c25b
Alireza6677 <alireza6677@gmail.com>
2019-11-08 13:55:40 +0330

    Project is now public

---
+++ PVdl.py
@@ -0,0 +1,223 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  7 21:02:09 2019
+
+@author: @TheWeirdDev
+"""
+
+import requests, json, sys, os, re
+from os import path
+import urllib.request
+from tqdm import tqdm
+import pycurl
+
+import argparse
+
+
+access_jwt = ""
+refresh_token = ""
+sess = requests.Session();
+
+def check_error(j):
+    if "errorCode" in j.keys():
+        print("Error:", j['message'])
+        exit(1)
+        
+SYMBOLS = {
+    'customary'     : ('B', 'K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y'),
+    'customary_ext' : ('byte', 'kilo', 'mega', 'giga', 'tera', 'peta', 'exa',
+                       'zetta', 'iotta'),
+    'iec'           : ('Bi', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi', 'Yi'),
+    'iec_ext'       : ('byte', 'kibi', 'mebi', 'gibi', 'tebi', 'pebi', 'exbi',
+                       'zebi', 'yobi'),
+}
+def human2bytes(s):
+    init = s
+    num = ""
+    while s and s[0:1].isdigit() or s[0:1] == '.':
+        num += s[0]
+        s = s[1:]
+    num = float(num)
+    letter = s.strip()
+    for name, sset in SYMBOLS.items():
+        if letter in sset:
+            break
+    else:
+        if letter == 'k':
+            # treat 'k' as an alias for 'K' as per: http://goo.gl/kTQMs
+            sset = SYMBOLS['customary']
+            letter = letter.upper()
+        else:
+            raise ValueError("can't interpret %r" % init)
+    prefix = {sset[0]:1}
+    for i, s in enumerate(sset[1:]):
+        prefix[s] = 1 << (i+1)*10
+    return int(num * prefix[letter])
+  
+
+class DownloadProgressBar(tqdm):
+    def curl_progress(self, total, existing, upload_t, upload_d):
+        self.total = total
+        self.update(existing - self.n)
+
+
+def curl_limit_rate(url, filename, rate_limit, desc):
+    """Rate limit in bytes"""
+    with DownloadProgressBar(unit='B', unit_scale=True,
+                               miniters=1, desc=desc) as pbar:
+        c = pycurl.Curl()
+        c.setopt(c.URL, url)
+        c.setopt(c.MAX_RECV_SPEED_LARGE, rate_limit)
+        if os.path.exists(filename):
+            file_id = open(filename, "ab")
+            c.setopt(c.RESUME_FROM, os.path.getsize(filename))
+        else:
+            file_id = open(filename, "wb")
+    
+        c.setopt(c.WRITEDATA, file_id)
+        c.setopt(c.NOPROGRESS, 0)
+        c.setopt(c.PROGRESSFUNCTION, pbar.curl_progress)
+        c.perform()
+    
+def download_url(url, output_path, limit_rate):
+    try:
+        desc = output_path.split(os.sep)[-1]
+        desc = desc[:desc.index(".mp4")]
+        #with DownloadProgressBar(unit='B', unit_scale=True,
+        #                    miniters=1, desc=desc) as t:
+        #   urllib.request.urlretrieve(url, filename=output_path, reporthook=t.update_to)
+        curl_limit_rate(url, output_path, limit_rate, desc)
+    except (Exception, KeyboardInterrupt) as e:
+        print(e)
+        exit(1)
+        
+def refresh():
+    global refresh_token
+    global access_jwt
+    login = sess.post("https://services.packtpub.com/auth-v1/users/me/tokens",
+                      json.dumps({"refresh": refresh_token}),
+                      headers={'Authorization': 'Bearer ' + access_jwt})
+    
+    login_data = json.loads(login.content)
+    check_error(login_data)
+    
+    refresh_token = login_data['data']['refresh']
+    access_jwt = login_data['data']['access']
+    
+def login(username, password):
+    login = sess.post("https://services.packtpub.com/auth-v1/users/tokens",
+                      json.dumps({"username": username,"password": password}))
+    login_data = json.loads(login.content)
+    check_error(login_data)
+    
+    global access_jwt
+    global refresh_token
+    refresh_token = login_data['data']['refresh']
+    access_jwt = login_data['data']['access']
+
+def get_video_url(vid_id, video_id):
+    worked = False
+    while(not worked):
+        url = "https://services.packtpub.com/products-v1/products/{}/{}".format(vid_id, video_id)
+        data = requests.get(url, headers={'Authorization': 'Bearer ' + access_jwt})
+        vid = json.loads(data.content)
+        if ("message" in vid.keys()) and vid["message"] == "jwt expired":
+            refresh()
+            continue
+        worked = True
+    if not ("data" in vid.keys()):
+        print("Error: Can't get video url")
+        exit(0)
+    return vid["data"]
+    
+def get_chapters(vid_id, limit_rate):
+    url = 'https://static.packt-cdn.com/products/{}/summary'.format(vid_id)
+    data = requests.get(url)
+    details = json.loads(data.content)
+    
+    if not ("title" in details.keys()):
+        print("Error: Wrong link. No video found.")
+        exit(1)
+    title = details["title"]
+    os.makedirs(title, 0o755, exist_ok=True)
+        
+    url = 'https://static.packt-cdn.com/products/{}/toc'.format(vid_id)
+    data = requests.get(url)
+    details = json.loads(data.content)
+    
+    if not ("chapters" in details.keys()):
+        print("Error: Wrong link. No video found.")
+        exit(1)
+        
+    print("Chapters found")
+    for x,i in enumerate(details['chapters']):
+        section = i['title']
+        s_path = "{}{}{:02}-{}".format(title, os.sep, x+1, section)
+        os.makedirs(s_path, 0o755, exist_ok=True)
+        print(section)
+        for y,c in enumerate(i['sections']):
+            chapter = c['title']
+            c_path = "{}{}{:02}-{}.mp4".format(s_path, os.sep , y+1, chapter)
+            video_id = i['id'] + '/' + c['id']
+            video_url = get_video_url(vid_id, video_id)
+            download_url(video_url, c_path, limit_rate)
+
+    
+def start_download(username, password, vid_id, limit_rate):
+    login(username, password)
+    r = requests.get('https://services.packtpub.com/users-v1/users/me/metadata', headers={'Authorization': 'Bearer ' + access_jwt})
+    metadata = json.loads(r.content)
+    check_error(metadata)
+    
+    name = metadata['name']
+    print("Logged in as '{} {}'".format(name['firstName'], name['lastName']))
+    print("mail: {}".format(metadata['mail']))
+    print("uuid: {}".format(metadata['uuid']))
+    
+    subscription = metadata['subscription']
+    if not (subscription['subscribed'] or subscription['freeTrial']):
+        print("You don't have a valid subscription")
+        exit(1)
+    
+    print("You have a valid Subscription")
+    
+    get_chapters(vid_id, limit_rate)
+    
+
+if __name__ == "__main__":
+    try:
+        parser = argparse.ArgumentParser(description='PacktPub Video Downloader', prog="PVdl.py")
+        required_args = parser.add_argument_group('required arguments')
+        required_args.add_argument('-u', '--username', help='Your Packt Username')
+        required_args.add_argument('-p', '--password', help='Your Password')
+        required_args.add_argument('-r', '--rate-limit', default = 0, help='Download rate limit')
+        required_args.add_argument('-l', '--link', help='Packt Video link')
+        
+        args = parser.parse_args()
+        if not (args.username or args.password or args.link):
+            parser.print_help()
+            exit(1)
+            
+        link = args.link
+        username = args.username
+        password = args.password
+        if(args.rate_limit):
+            limit_rate = human2bytes(args.rate_limit)
+        else:
+            limit_rate = 0
+            
+        vid_id = re.findall(r"https://.*packtpub.*video/.*/(\d+)/?$", link)
+        if len(vid_id) < 1:
+            print("Error: Invalid link")
+            exit(1)
+        vid_id = vid_id[0]
+        start_download(username, password, vid_id, limit_rate)
+    except KeyboardInterrupt:
+        print("Interrupted")
+        exit(1)
+    
+    
+    
+    
+
---
+++ Readme.md
@@ -0,0 +1,42 @@
+# PVdl: PacktPub Video Downloader
+
+The Easiest way to download Packt videos
+
+**Notice: You need a Packt account with subscription or free trial to download videos**
+
+
+# How to use
+
+Clone this repository, then run the script. For example:
+
+```bash
+chmod +x ./PVdl.py
+./PVdl.py -u username@example.com -p Passw0rd -l "https://subscription.packtpub.com/video/programming/9781788834995"
+```
+
+Usage:
+
+```
+usage: PVdl.py [-h] [-u USERNAME] [-p PASSWORD] [-r RATE_LIMIT] [-l LINK]
+
+PacktPub Video Downloader
+
+optional arguments:
+  -h, --help            show this help message and exit
+
+required arguments:
+  -u USERNAME, --username USERNAME
+                        Your Packt Username
+  -p PASSWORD, --password PASSWORD
+                        Your Password
+  -r RATE_LIMIT, --rate-limit RATE_LIMIT
+                        Download rate limit
+  -l LINK, --link LINK  Packt Video link
+
+```
+
+# License
+
+PacktPub Video Downloader is a free software and is licensed under GNU Public License v3+
+
+For more information see [LICENSE](LICENSE)

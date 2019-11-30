5eca910ab750ef7f9e000df105218322e4cb6b43
Alireza6677 <alireza6677@gmail.com>
2019-11-30 11:46:28 +0330

    Fixed backslash in regex

--- PVdl.py
+++ PVdl.py
@@ -153,7 +153,7 @@ def get_chapters(vid_id, limit_rate):
         print_err("Error: Wrong link. No video found.")
         
     title = details["title"]
-    title = re.sub("[<>|/\?*]", "_" ,title);
+    title = re.sub(r"[<>|/\\?*]", "_" ,title);
     os.makedirs(title, 0o755, exist_ok=True)
         
     url = 'https://static.packt-cdn.com/products/{}/toc'.format(vid_id)
@@ -166,13 +166,13 @@ def get_chapters(vid_id, limit_rate):
     all_chapters = details['chapters']
     for x,i in enumerate(all_chapters):
         section = i['title']
-        section = re.sub("[<>|/\?*]", "_" , section)
+        section = re.sub(r"[<>|/\\?*]", "_" , section)
         s_path = "{}{}{:02}-{}".format(title, os.sep, x+1, section)
         os.makedirs(s_path, 0o755, exist_ok=True)
         print(bcolors.OKGREEN + "\nChapter {}/{}:".format(x+1, len(all_chapters)), section, "\n" + bcolors.ENDC)
         for y,c in enumerate(i['sections']):
             chapter = c['title']
-            chapter = re.sub("[<>|/\?*]", "_" , chapter)
+            chapter = re.sub(r"[<>|/\\?*]", "_" , chapter)
             c_path = "{}{}{:02}-{}.mp4".format(s_path, os.sep , y+1, chapter)
             video_id = i['id'] + '/' + c['id']
             video_url = get_video_url(vid_id, video_id)
